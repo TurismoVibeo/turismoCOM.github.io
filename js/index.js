d8f25a2ad5bbf8db6034e8cde7d8cfd5cf8e4bf3
TurismoVibeo <96960765+TurismoVibeo@users.noreply.github.com>
2022-07-18 13:56:29 -0700

    Added socials

--- js/index.js
+++
@@ -1,5 +0,0 @@
-function changeBackgroundImg(){
-    document.body.style.backgroundImage = "url('https://www.google.com/imgres?imgurl=https%3A%2F%2Fwallpaperaccess.com%2Ffull%2F4220753.jpg&imgrefurl=https%3A%2F%2Fwallpaperaccess.com%2Fgreen-binary-code&tbnid=M8OWjc9WJyRMPM&vet=12ahUKEwi_l-irtoL5AhVygXMKHbJwAVAQMygAegUIARDUAQ..i&docid=AaAZYwdjCRQXtM&w=1920&h=1080&q=green%20binary%20background&client=opera-gx&ved=2ahUKEwi_l-irtoL5AhVygXMKHbJwAVAQMygAegUIARDUAQ')";
-}
-
-document.addEventListener("DOMContentLoaded", changeBackgroundImg());
